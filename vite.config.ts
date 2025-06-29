2400343b64da228d0be3954a0ba673a530f66b1f
Ayaan Pupala <35133058+Ayaan-P@users.noreply.github.com>
2025-06-28 21:39:03 -0400

    Fix Mixed Content Error with Vite Proxy

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,13 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   build: {
     outDir: 'dist',
